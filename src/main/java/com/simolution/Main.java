aacef3bc731dd869efc2da52751142518a66f84f
MehmetScgn <mehmetascgn@gmail.com>
2026-01-04 05:18:18 +0200

    feat: add main application entry point

---
+++ src/main/java/com/simolution/Main.java
@@ -0,0 +1,56 @@
+package com.simolution;
+
+import com.simolution.kernel.genome.GeneBuilder;
+import com.simolution.kernel.genome.GenomeCompiler;
+import com.simolution.kernel.layout.CompiledConnection;
+import com.simolution.kernel.layout.NodeLayout;
+import com.simolution.kernel.runtime.Kernel;
+
+public class Main {
+
+    static void main() {
+        short wConstToAdd   = 2048; // ~ +0.25
+        short wAddToDelay   = 512;  // ~ +0.0625
+        short wDelayToAdd   = 512;  // ~ +0.0625
+        short wAddToAction  = 512;  // ~ +0.0625
+
+        int[] genome = {
+
+                // CONST -> ADD
+                GeneBuilder.fromSensor(NodeLayout.Sensor.CONST)
+                           .toInternal(NodeLayout.Internal.ADD)
+                           .weightRaw(wConstToAdd)
+                        .build(),
+
+                // ADD -> DELAY
+                GeneBuilder.fromInternal(NodeLayout.Internal.ADD)
+                           .toInternal(NodeLayout.Internal.DELAY)
+                           .weightRaw(wAddToDelay)
+                        .build(),
+
+                // DELAY -> ADD  (feedback loop, 1-tick delayed)
+                GeneBuilder.fromInternal(NodeLayout.Internal.DELAY)
+                           .toInternal(NodeLayout.Internal.ADD)
+                           .weightRaw(wDelayToAdd)
+                        .build(),
+
+                // ADD -> ACTION
+                GeneBuilder.fromInternal(NodeLayout.Internal.ADD)
+                           .toAction(NodeLayout.Action.Y)
+                           .weightRaw(wAddToAction)
+                        .build()
+        };
+
+        CompiledConnection[] connections = GenomeCompiler.compile(genome, 0);
+
+        Kernel kernel = new Kernel(connections);
+        kernel.dumpWiring();
+
+
+        for (int i = 0; i < 10; i++) {
+            kernel.tick();
+            kernel.debugDump();
+        }
+    }
+
+}
